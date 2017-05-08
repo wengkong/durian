5b35a843113c2c54f9d637b8f2cb44d4061c7a36
Weng Kong Kam <weng-kong.kam@hpe.com>
2017-05-08 12:11:05 +0800

    Test push integration

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ var request = require('supertest');
 var app = require('../app.js');
 
 describe('GET /users', function() {
-  it('respond with respond with a resource', function(done) {
+  it('respond with respond with a resource...', function(done) {
     request(app).get('/users').expect('respond with a resource', done);
   })
 });
